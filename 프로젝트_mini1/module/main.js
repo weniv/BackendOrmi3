9e8f5a1ff1e554425ca461a56f84730932a8feef
paullabkorea <paullabkorea@gmail.com>
2023-08-29 14:03:57 +0900

    0829

---
+++ 프로젝트_mini1/module/main.js
@@ -0,0 +1,5 @@
+// main.js
+import name, { sayHello } from './module.js';
+
+document.body.innerHTML = 'Hello, ' + name;
+sayHello();
---
+++ 프로젝트_mini1/module/module.js
@@ -0,0 +1,8 @@
+// module.js
+const name = 'Hojun Lee';
+function sayHello() {
+    console.log('Hello!');
+}
+
+export default name;
+export { sayHello };
