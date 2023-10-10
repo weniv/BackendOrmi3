7dfee5de1270f55acc28efc098bcb249821228a1
paullabkorea <paullabkorea@gmail.com>
2023-10-10 14:29:36 +0900

    1010

---
+++ Django/1010/mysite/blog/urls.py
@@ -0,0 +1,9 @@
+# blog > urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.blog, name='blog'),
+    path('<int:pk>/', views.post, name='post'),
+]
---
+++ Django/1010/mysite/main/views.py
@@ -0,0 +1,11 @@
+# main > views.py
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
